8362fa9fe68ce2324fa2cca75955e921f31e4609
gv4383 <gregory.vargas18@gmail.com>
2019-03-07 21:13:57 -0600

    Added new toy problem

---
+++ count_the_digit.js
@@ -0,0 +1,21 @@
+// url: https://www.codewars.com/kata/count-the-digit/javascript
+
+// My solution
+// time complexity:
+// space complexity:
+
+function nbDig(n, d) {
+  let count = 0;
+
+  for (let i = 0; i <= n; i++) {
+    let squaredNum = [...String(i ** 2)];
+
+    for (let j = 0; j < squaredNum.length; j++) {
+      if (squaredNum[j] == d) {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
+}
